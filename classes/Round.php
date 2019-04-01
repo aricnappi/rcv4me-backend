9e1c393cd8b3be67a8b5ae8c5dad2e6d8a85f92f
Aric Nappi <49166157+aricnappi@users.noreply.github.com>
2019-04-01 18:02:12 -0400

    Update Round.php

--- classes/Round.php
+++ classes/Round.php
@@ -1,12 +1,4 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 declare(strict_types=1);
 include 'Pile.php';
 
