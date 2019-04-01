cbc9b5a5458eafdfe14eec7b41540a69829a6430
Aric Nappi <49166157+aricnappi@users.noreply.github.com>
2019-04-01 17:57:44 -0400

    Update get_poll.php

--- get_poll.php
+++ get_poll.php
@@ -1,11 +1,4 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 // Connect to database
 include '/var/www/html/phpscripts/common/initialization.php';
 
