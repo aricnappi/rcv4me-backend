fe009e8dbcdd184f39e3c6644fe31eeaedf3b12d
Aric Nappi <49166157+aricnappi@users.noreply.github.com>
2019-04-01 18:00:14 -0400

    Update initialization.php

--- common/initialization.php
+++ common/initialization.php
@@ -1,11 +1,4 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 // Error reporting when debugging
 //error_reporting(E_ALL | E_STRICT);
 //Error reporting for production
